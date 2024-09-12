2c9e39d09a0c246ecba0402b06d6db083109a264
Noyon Ahammed Sojon <ahammedsojon000@gmail.com>
2024-09-12 18:01:51 +0600

    handleZodError function created

---
+++ src/app/interfaces/error.ts
@@ -0,0 +1,4 @@
+export type TErrorScources = {
+  path: string;
+  message: string;
+}[];
--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -2,21 +2,48 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorScources } from '../interfaces/error';
+import config from '../config';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = 500;
-  const message = err.message || 'Something went wrong!';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = 500;
+  let message = err.message || 'Something went wrong!';
+
+  let errorSources: TErrorScources = [
+    {
+      path: '',
+      message: err.message || 'Somtning went wrong!',
+    },
+  ];
+  const handleZodError = () => {
+    statusCode = 400;
+    message = 'Validation Error';
+    const errorSources = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path.pop(),
+        message: issue?.message,
+      };
+    });
+    return {
+      statusCode,
+      message,
+      errorSources,
+    };
+  };
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError();
+    message = simplifiedError.message;
+    statusCode = simplifiedError.statusCode;
+    errorSources = simplifiedError.errorSources;
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    stack: config.node_env === 'development' ? err.stack : null,
   });
 };
 

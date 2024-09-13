eb442787e1072cee948098e8000de50d8c1ba210
Noyon Ahammed Sojon <ahammedsojon000@gmail.com>
2024-09-13 15:18:36 +0600

    filter search query added in student

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -1,49 +1,72 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
-import { ZodError, ZodIssue } from 'zod';
-import { TErrorScources } from '../interfaces/error';
+/* eslint-disable no-unused-vars */
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 import config from '../config';
+import AppError from '../errors/AppError';
+import handleCastError from '../errors/handleCastError';
+import handleDuplicateError from '../errors/handleDuplicateError';
+import handleValidationError from '../errors/handleValidationError';
+import handleZodError from '../errors/handleZodError';
+import { TErrorSources } from '../interface/error';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  //setting default values
   let statusCode = 500;
-  let message = err.message || 'Something went wrong!';
-
-  let errorSources: TErrorScources = [
+  let message = 'Something went wrong!';
+  let errorSources: TErrorSources = [
     {
       path: '',
-      message: err.message || 'Somtning went wrong!',
+      message: 'Something went wrong',
     },
   ];
-  const handleZodError = () => {
-    statusCode = 400;
-    message = 'Validation Error';
-    const errorSources = err.issues.map((issue: ZodIssue) => {
-      return {
-        path: issue?.path.pop(),
-        message: issue?.message,
-      };
-    });
-    return {
-      statusCode,
-      message,
-      errorSources,
-    };
-  };
+
   if (err instanceof ZodError) {
-    const simplifiedError = handleZodError();
-    message = simplifiedError.message;
-    statusCode = simplifiedError.statusCode;
-    errorSources = simplifiedError.errorSources;
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err instanceof AppError) {
+    statusCode = err?.statusCode;
+    message = err.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message,
+      },
+    ];
+  } else if (err instanceof Error) {
+    message = err.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message,
+      },
+    ];
   }
 
+  //ultimate return
   return res.status(statusCode).json({
     success: false,
     message,
     errorSources,
-    stack: config.node_env === 'development' ? err.stack : null,
+    err,
+    stack: config.node_env === 'development' ? err?.stack : null,
   });
 };
 
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -5,8 +5,57 @@ import httpStatus from 'http-status';
 import { User } from '../user/user.model';
 import { TStudent } from './student.interface';
 
-const getAllStudentsFromDB = async () => {
-  const result = await Student.find();
+const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
+  let searchTerm = '';
+  if (query?.searchTerm) {
+    searchTerm = query.searchTerm as string;
+  }
+
+  const searchableFields = ['email', 'name.firstName'];
+
+  const searchQuery = Student.find({
+    $or: searchableFields.map((field) => ({
+      [field]: { $regex: searchTerm, $optoins: 'i' },
+    })),
+  });
+
+  const queryObj = { ...query };
+
+  const excludeFields = ['searchTerm', 'sort', 'limit', 'page', 'fields'];
+  excludeFields.forEach((el) => delete queryObj[el]);
+
+  const filterQuery = searchQuery
+    .find(queryObj)
+    .populate('academicSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    });
+  let sort = '-createdAt';
+  if (query?.sort) {
+    sort = query.sort as string;
+  }
+  const sortQuery = filterQuery.find().sort(sort);
+  let limit = 1;
+  if (query?.limit) {
+    limit = Number(query.limit);
+  }
+  let skip = 0;
+  let page = 1;
+  if (query?.page) {
+    page = Number(query.page);
+    skip = (page - 1) * limit;
+  }
+  const limitQuery = sortQuery.skip(skip).limit(limit);
+
+  let fields = '-__v';
+  if (query?.fields) {
+    fields = (query.fields as string).split(',').join(' ');
+  }
+
+  const result = await limitQuery.select(fields);
   return result;
 };
 
